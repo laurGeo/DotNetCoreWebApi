c8a57244b905328fdc387bae72d8cdb45c0580d4
laurGeo <laureng5289@gmail.com>
2021-10-03 17:48:02 +0100

    Add gitignore, add pizza service

---
+++ Models/Pizza.cs
@@ -0,0 +1,7 @@
+namespace DotNetCoreWebApi.Models{
+    public class Pizza {
+        public int Id {get;set;}
+        public string Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
