948f107200abd6932f369230cd9477d1f520b6b5
laurGeo <laureng5289@gmail.com>
2021-10-03 17:59:55 +0100

    Add get pizza actions

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using DotNetCoreWebApi.Models;
+using DotNetCoreWebApi.Services;
+
+namespace ContosoPizza.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+        }
+
+        // GET all action
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+        // GET by Id action
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id){
+            var pizza = PizzaService.Get(id);
+            if(pizza is null) return NotFound();
+            return pizza;
+        }
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
